a62a4d981acd9f7e79db3132cfeddca35fa01ca6
Emiel Van Severen <emielvanseveren@outlook.com>
2019-10-05 09:33:31 +0000

    Add globalstyle to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Routing from './components/Routing'
 import { ThemeProvider } from './hooks/themeContext'
+import Style from './constants/globalStyle'
 
 export default function App(){
   return (
     <ThemeProvider>
+      <Style/>
       <Routing/>
     </ThemeProvider>
   )
