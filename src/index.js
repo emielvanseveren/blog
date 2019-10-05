718baedb0c2e71d6698a2f00615d69808fc467bc
Emiel Van Severen <emielvanseveren@outlook.com>
2019-10-05 07:54:37 +0000

    Add themeprovider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { ThemeProvider } from 'styled-components'
+
+
 import App from './App'
 
 render(
-  <App/>,
+  <ThemeProvider>
+  <App/>
+  </ThemeProvider>,
   document.getElementById('root'))
